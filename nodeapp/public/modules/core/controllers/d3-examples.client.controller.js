cb9d479b9eca72166687fd49c46217fa4d260edf
bberzinskas <bberzinskas@stanleygibbons.com>
2014-08-13 03:07:11 -0400

    examples

--- nodeapp/public/modules/core/controllers/d3-examples.client.controller.js
+++ nodeapp/public/modules/core/controllers/d3-examples.client.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('core').controller('D3ExamplesController', ['$scope', '$http',
+angular.module('core').controller('D3GitController', ['$scope', '$http',
 	function($scope, $http) {
   		// initialize the model
     $scope.user = 'digirithm';
@@ -93,6 +93,55 @@ angular.module('core').controller('D3ExamplesController', ['$scope', '$http',
         }
       });
   	}
+  }
+
+]);
+
+angular.module('core').controller('D3ChoroplethController', ['$scope', '$http',
+  function($scope, $http) {
+  // chorpleth
+
+    var width = 960,
+        height = 600;
+     
+    var rateById = d3.map();
+     
+    var quantize = d3.scale.quantize()
+        .domain([0, .15])
+        .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
+     
+    var projection = d3.geo.albersUsa()
+        .scale(1280)
+        .translate([width / 2, height / 2]);
+     
+    var path = d3.geo.path()
+        .projection(projection);
+     
+    var svg = d3.select("#choropleth_demo").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+     
+    queue()
+        .defer(d3.json, "/testdata/us.json")
+        .defer(d3.tsv, "/testdata/unemployment.tsv", function(d) { rateById.set(d.id, +d.rate); })
+        .await(ready);
+     
+    function ready(error, us) {
+      svg.append("g")
+          .attr("class", "counties")
+        .selectAll("path")
+          .data(topojson.feature(us, us.objects.counties).features)
+        .enter().append("path")
+          .attr("class", function(d) { return quantize(rateById.get(d.id)); })
+          .attr("d", path);
+     
+      svg.append("path")
+          .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+          .attr("class", "states")
+          .attr("d", path);
+    }
+     
+    d3.select(self.frameElement).style("height", height + "px");
   }
 ]);
 
